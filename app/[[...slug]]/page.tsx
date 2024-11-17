95b1c81c1774452bc3250ae4e85197a71a3475e6
leo <leo@gmail.com>
2024-11-17 20:46:53 +0800

    修复bug

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -5,7 +5,6 @@ import Gameframe from "@/components/my/gameframe";
 import GameItem from "@/components/my/game-item";
 import { notFound, redirect } from "next/navigation";
 import { MDXRemote } from "next-mdx-remote/rsc";
-import Head from "next/head";
 
 interface PropsType {
   params: Promise<{ slug: string }>;
@@ -19,22 +18,26 @@ export async function generateMetadata({
 
   if (!gameInfo) {
     return {
-      title: "404",
-      description: "Page not found!",
+      title: "Page not found",
+      description: "Sorry, This Game isn't available.",
     };
   }
+
+  const dimain = "https://www.fnfsprunki.top/";
+  const canonicalUrl = slug ? dimain + slug[0] : dimain;
 
   const { metadata } = getMdxContent(`${gameInfo.slug}.mdx`);
   return {
     title: metadata.title,
     keywords: metadata.keywords,
     description: metadata.description,
+    alternates: {
+      canonical: canonicalUrl,
+    },
   };
 }
 
 const getGameInfo = (slug: string) => {
-  // console.log("slug=", slug);
-
   return GameList.find((item) => item.slug === slug);
 };
 
@@ -54,47 +57,39 @@ export default async function GamePage({ params }: PropsType) {
     notFound();
   }
 
-  const dimain = "https://www.fnfsprunki.top/";
-  const canonicalUrl = slug ? dimain + slug[0] : dimain;
-
   const { content } = getMdxContent(`${gameInfo.slug}.mdx`);
 
   const popularGames = randomGameList();
 
   return (
-    <>
-      <Head>
-        <link rel="canonical" href={canonicalUrl} key="canonical" />
-      </Head>
-      <main className="flex-grow flex flex-col lg:flex-row items-start my-10  gap-8 w-10/12 max-w-7xl mx-auto">
-        {/* 左边部分 */}
-        <div className="w-full lg:w-3/4">
-          {/* 游戏框 */}
-          <div className="flex bg-gray-800 rounded-lg shadow-md w-full h-full min-h-[600px] mb-8 overflow-hidden">
-            <Gameframe gameInfo={gameInfo} />
-          </div>
-
-          {/* 游戏介绍 */}
-          <article
-            style={{ maxWidth: "100%", borderRadius: "0.5rem" }}
-            className="prose lg:prose-xl prose-invert prose-h2:text-pink-500 bg-gray-800 p-8 rounded-lg shadow-md w-full "
-          >
-            {gameInfo && <MDXRemote source={content} />}
-          </article>
+    <main className="flex-grow flex flex-col lg:flex-row items-start my-10  gap-8 w-10/12 max-w-7xl mx-auto">
+      {/* 左边部分 */}
+      <div className="w-full lg:w-3/4">
+        {/* 游戏框 */}
+        <div className="flex bg-gray-800 rounded-lg shadow-md w-full h-full min-h-[600px] mb-8 overflow-hidden">
+          <Gameframe gameInfo={gameInfo} />
         </div>
 
-        {/* 右侧推荐游戏列表 */}
-        <aside className="w-full lg:w-1/4 ">
-          <div className="bg-gray-800 p-4 rounded-lg mb-6">
-            <h2 className="text-2xl font-bold text-pink-500">Popular Games</h2>
-          </div>
-          <div className="grid grid-cols-2 gap-x-2 gap-y-3">
-            {popularGames?.map((game, index) => (
-              <GameItem gameInfo={game} key={index} />
-            ))}
-          </div>
-        </aside>
-      </main>
-    </>
+        {/* 游戏介绍 */}
+        <article
+          style={{ maxWidth: "100%", borderRadius: "0.5rem" }}
+          className="prose lg:prose-xl prose-invert prose-h2:text-pink-500 bg-gray-800 p-8 rounded-lg shadow-md w-full "
+        >
+          {gameInfo && <MDXRemote source={content} />}
+        </article>
+      </div>
+
+      {/* 右侧推荐游戏列表 */}
+      <aside className="w-full lg:w-1/4 ">
+        <div className="bg-gray-800 p-4 rounded-lg mb-6">
+          <h2 className="text-2xl font-bold text-pink-500">Popular Games</h2>
+        </div>
+        <div className="grid grid-cols-2 gap-x-2 gap-y-3">
+          {popularGames?.map((game, index) => (
+            <GameItem gameInfo={game} key={index} />
+          ))}
+        </div>
+      </aside>
+    </main>
   );
 }
